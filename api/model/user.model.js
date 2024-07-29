ece122c214ee512d689c9a74c45bbd07b0da82ca
MindCraftGT <dnyaks32@gmail.com>
2024-07-29 22:59:41 +0300

    update user model to include email

--- api/model/user.model.js
+++ api/model/user.model.js
@@ -2,17 +2,25 @@ import mongoose from "mongoose";
 
 
 //creating a userschema
-const userSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		require: true,
-		unique: true,
+const userSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			require: true,
+			unique: true,
+		},
+		email: {
+			type: String,
+			require: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			require: true,
+		},
 	},
-	password: {
-		type: String,
-		require: true,
-	},
-}, { timestamps: true });
+	{ timestamps: true },
+);
 
 //creating of a user model
 const User = mongoose.model('User', userSchema);
