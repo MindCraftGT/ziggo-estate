b09ec5c15ec3e7f737f66f2ec033707b75b38d2f
MindCraftGT <dnyaks32@gmail.com>
2024-07-29 22:18:22 +0300

    created and connected to mongodb for developing the backend api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(
+	"mongodb+srv://messycoder32:nasa2017@zigoestate.r7addq4.mongodb.net/zigo-estate?retryWrites=true&w=majority&appName=zigoestate"
+).then(() => {
+	console.log("Succefully Connected to MongoDb!")
+}).catch((err) =>{
+	console.Console.log(err);
+})
 
 const app = express();
 
