4ef4d2aa6df3f4526ab60dc85009921f489205b2
MindCraftGT <dnyaks32@gmail.com>
2024-08-01 23:11:42 +0300

    added redux persist function

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import useReducer from "./user/userSlice";
+import { persistReducer } from 'redux-persist';
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+//add redux-persist to browser losing data and signing off the user whenever the page is reloaded
+const rootReducer = combineReducers({user: useReducer});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-	reducer: {user: useReducer},
+	reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
