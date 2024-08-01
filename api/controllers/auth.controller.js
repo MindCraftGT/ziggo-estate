ba5eb270d6b0264e80a55ca109a21851d95c8a3e
MindCraftGT <dnyaks32@gmail.com>
2024-08-01 12:40:18 +0300

    added a sign in route and fixed jsonwebtoken for the user login session by destructuring of the hashedpassword from appearing.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../model/user.model.js";
 import bcrypt from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -11,6 +12,22 @@ export const signup = async (req, res, next) => {
 		res.status(201).json({ message:"New User created Successfully" });
     } catch (error) {
         next(error);
+    }  
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body; // authenticate a user using their registered email and password
+    try {
+       const validUser = await User.findOne({ email });
+       if(!validUser) next(errorHandler(404, 'User not found'));
+       const validPassword = bcrypt.compareSync(password, validUser.password); //compare password offered by the user and match it with that of the valid user in the database
+       if(!validPassword) next(errorHandler(401, 'Wrong credentials!'));
+       const token = jwt.sign({ id: validUser._id }, `${process.env.JWT_SECRET}`) //handling cookie sessions for the user
+       const { password: pass, ...rest } = validUser._doc //only sharing user information except password to the cookie session
+       res.cookie('access_token', token, { httpOnly: true })
+       .status(200)
+       .json(rest);
+    } catch (error) {
+       next(error) // next is adopted middleware from index.js file to handle errors 
     }
-    
 }
--- api/index.js
+++ api/index.js
@@ -1,35 +1,39 @@
-import express from "express";
-import mongoose from 'mongoose';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
 import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
-mongoose.connect(
-	"mongodb+srv://messycoder32:nasa2017@zigoestate.r7addq4.mongodb.net/zigo-estate?retryWrites=true&w=majority&appName=zigoestate"
-).then(() => {
-	console.log("Succefully Connected to MongoDb!")
-}).catch((err) =>{
-	console.Console.log(err);
-})
+mongoose
+	.connect(`${process.env.MONGO}`)
+	.then(() => {
+		console.log(
+			"Succefully Connected to MongoDb!",
+		);
+	})
+	.catch((err) => {
+		console.Console.log(err);
+	});
 
 const app = express();
 
 app.use(express.json());
 
 //creating api routes
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 //creating a middleware for handling errors
 app.use((err, req, res, next) => {
 	const statusCode = err.statusCode || 500;
-	const message = err.message || 'Internal Server Error';
+	const message =
+		err.message || "Internal Server Error";
 	return res.status(statusCode).json({
 		success: false,
 		statusCode: statusCode,
-		message
+		message,
 	});
 });
 
