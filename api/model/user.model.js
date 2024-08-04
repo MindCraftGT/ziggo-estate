be0661714d9eac5a2637699abf9a644020c9219c
MindCraftGT <dnyaks32@gmail.com>
2024-08-04 22:05:48 +0300

    Advancing the development of the profile UI

--- api/model/user.model.js
+++ api/model/user.model.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema(
 		avatar: {
 			type: String,
 			default:
-				"https://img.freepik.com/free-psd/3d-render-avatar-character_23-2150611722.jpg?w=996&t=st=1722632238~exp=1722632838~hmac=f5ac7b436e0a1700616c1cd77c3b8098c0948be1707c1fc9ff357d59ffacbec1"
+				"https://www.vecteezy.com/free-vector/profile-avatar"
 		},
 	},
 	{ timestamps: true },
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { useSelector } from "react-redux";
 
 export default function Profile() {
+  const { currentUser } = useSelector(state => state.user);
   return (
-    <div>Profile</div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-8">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img src={currentUser.avatar} alt="profile"
+        className="rounded-full h-25 w-25 object-cover cursor-pointer
+        self-center mt-3"/>
+        <input type="text" placeholder="Username" id="username"
+        className="border rounded-lg p-3 "/>
+        <input type="text" placeholder="Email" id="email"
+        className="border rounded-lg p-3 "/>
+        <input type="text" placeholder="Password" id="password"
+        className="border rounded-lg p-3 "/>
+        <button className="bg-slate-800 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80">Update</button>
+      </form>
+      <div className="flex justify-between items-center p-4 mt-4">
+        <span className="text-red-500 hover:text-red-800 cursor-pointer">Delete Account</span>
+        <span className="text-red-500 hover:text-red-800 cursor-pointer">Sign Out</span>
+      </div>
+    </div>
   )
 }
