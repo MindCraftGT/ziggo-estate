24505642e7a7681a6170c7074c53b577ec0eccca
MindCraftGT <dnyaks32@gmail.com>
2024-08-04 19:45:32 +0300

    minor comments

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { Outlet, Navigate } from 'react-router-dom';
 
+//making the profile page private
 export default function PrivateRoute() {
     const { currentUser} = useSelector(state => state.user);
   return currentUser ? <Outlet /> : <Navigate to='/sign-in' />
