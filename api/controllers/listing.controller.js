612f2d76ca76fe6a4e2105f219f4f7984b5c27e7
MindCraftGT <dnyaks32@gmail.com>
2024-08-10 23:47:15 +0300

    updated the update listing route in the api/listing server.

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../model/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 // Create a new listing
 export const createListing = async(req, res, next) => {
@@ -28,3 +29,27 @@ export const deleteListing = async(req, res, next) => {
         next(error);
     }
 }
+
+// Update a listing
+export const updateListing = async(req, res, next) => {
+    const listing = await Listing.findById(re.params.id);
+
+    if(!listing) {
+        return next(errorHandler(404, 'Listing not found!'));
+    }
+
+    if(req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'You can only update your own listings!'));
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+    }
+};
