d40e17bfa653d0acea7570b67c05cf953d78d2d9
MindCraftGT <dnyaks32@gmail.com>
2024-08-16 14:28:52 +0300

    completed About.jsx UI page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,17 @@
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-emerald-800">About Zigo Real Estate Company</h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At ex eos numquam ad magnam doloribus quo vero iusto, molestiae doloremque incidunt aut pariatur atque soluta voluptates voluptatum nihil minima dolorem? Nam, soluta. Omnis numquam rerum cum, dolor maiores aut incidunt iste molestiae, minima quod eum inventore ipsa animi suscipit at.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem velit, mollitia alias dicta assumenda tenetur porro a provident incidunt inventore cupiditate, repellendus vitae delectus voluptate ducimus, debitis quod consequatur deleniti nam modi! Recusandae ipsam reprehenderit quasi pariatur dolorum eaque deleniti ex quisquam velit iure. Quae porro quasi harum veniam aliquam?
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eaque officia ad perferendis maiores qui ratione, facilis ab, ea delectus non a sint laboriosam dicta, nobis molestiae dolores est distinctio! Ratione maiores, neque amet hic totam optio quaerat temporibus, unde dolore tenetur dignissimos pariatur. Ipsa facilis neque molestias veniam? Voluptatum.
+      </p>
+    </div>
   )
 }
