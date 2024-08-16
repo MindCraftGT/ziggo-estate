54f34bd5d39e4de7b5d8454a4ceb4395c87ad675
MindCraftGT <dnyaks32@gmail.com>
2024-08-16 14:57:10 +0300

    Updated the Index.js file in the Api route to be able to render the index.html file when other routes are responding. And also the changes will be crucial in the production mode.

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -19,6 +20,11 @@ mongoose
 		console.Console.log(err);
 	});
 
+	// Serving static files from the React app
+	const __dirname = path.dirname(
+		new URL(import.meta.url).pathname,
+	);
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -27,6 +33,15 @@ app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+//Rendering to Cloud Storage
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+//Handling all routes not found
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 //creating a middleware for handling errors
 app.use((err, req, res, next) => {
