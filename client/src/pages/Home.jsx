22b2289bf68b2322dd86fb3742c5ee34caa9f6c3
MindCraftGT <dnyaks32@gmail.com>
2024-08-17 16:32:31 +0300

    minor updates on json files

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -51,7 +51,7 @@ export default function Home() {
               We have a wide variety of properties to choose from.
             </p>
           </div>
-          <Link to="/search" className="text-xs sm:text-sm text-emerald-500 font-bold hover:underline">
+          <Link to="/search" className="text-xs sm:text-sm text-emerald-600 font-bold hover:underline">
             Let&apos;s get started...
           </Link>
         </div>
@@ -73,7 +73,7 @@ export default function Home() {
       </Swiper>
 
       {/*Listing results for offer, sales and rent */}
-      <div className="max-w-6xl mx-auto p-3 flex-col gap-8 my-10">
+      <div className="max-w-6xl mx-auto p-4 flex-col gap-8 my-10">
         {offerListings && offerListings.length > 0 && (
           <div className="">
             <div className="my-3">
@@ -106,7 +106,7 @@ export default function Home() {
         )}
         {rentListings && rentListings.length > 0 && (
           <div className="">
-            <div className="my-3">
+            <div className="my-4">
               <h2 className='text-2xl font-semibold text-slate-600 text-center'>Recent Property for Rental</h2>
               <Link to={'/search?type=rent'} className='text-sm text-emerald-800 hover:underline'>
                 Show More Rentals
