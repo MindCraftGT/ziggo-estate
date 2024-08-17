45a827913e1c950c6acd4884ee3a62dc7e809ff1
MindCraftGT <dnyaks32@gmail.com>
2024-08-17 16:51:37 +0300

    root directory update

--- api/index.js
+++ api/index.js
@@ -1,62 +1,69 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-import cookieParser from 'cookie-parser';
-import listingRouter from './routes/listing.route.js';
-import path from 'path';
+import cookieParser from "cookie-parser";
+import listingRouter from "./routes/listing.route.js";
+import path from "path";
 
 dotenv.config();
 
+// MongoDB connection
 mongoose
 	.connect(`${process.env.MONGO}`)
-	.then(() => {
+	.then(() =>
 		console.log(
-			"Succefully Connected to MongoDb!",
-		);
-	})
-	.catch((err) => {
-		console.Console.log(err);
-	});
+			"Successfully Connected to MongoDB!",
+		),
+	)
+	.catch((err) => console.log(err));
 
-	// Serving static files from the React app
-	const __dirname = path.dirname(
-		new URL(import.meta.url).pathname,
-	);
-
+// Initialize Express app
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-//creating api routes
+// Define API routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-//Rendering to Cloud Storage
+// Serve static files from the React frontend app
+const __dirname = path.resolve(); // Get the root directory
+app.use(
+	express.static(
+		path.join(__dirname, "/client/build"),
+	),
+);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-//Handling all routes not found
+// Fallback to index.html for any routes not found
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+	res.sendFile(
+		path.join(
+			__dirname,
+			"client",
+			"build",
+			"index.html",
+		),
+	);
 });
 
-//creating a middleware for handling errors
+// Error handling middleware
 app.use((err, req, res, next) => {
 	const statusCode = err.statusCode || 500;
 	const message =
 		err.message || "Internal Server Error";
 	return res.status(statusCode).json({
 		success: false,
-		statusCode: statusCode,
+		statusCode,
 		message,
 	});
 });
 
+// Start server
 app.listen(3000, () => {
 	console.log(
-		"Serve Running on Port 3000, Nice Toast!",
+		"Server running on port 3000, Nice Toast!",
 	);
 });
